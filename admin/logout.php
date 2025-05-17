8e828ee0014f78799f1062d32bccb309aaeccb5b
Vitu <kilys2@outlook.com>
2025-05-16 22:28:50 -0300

    apagando coisas

--- admin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php include_once './config/config.php';
-
-unset($_SESSION['userLogged']);
-unset($_SESSION['userId']);
-unset($_SESSION['userName']);
-
-header("Location: login.php");
--- admin/pages/home.php
+++
@@ -1,5 +0,0 @@
-<div class="home_content">
-    <h1>                   
-        Seja bem vindo, <b><?= $_SESSION['userName'] ?></b> <?= TITLE; ?>
-    </h1>
-</div>
